c155ce4b08834ac68586f1058b749f0c631e2387
Lingy12 <russellgy1229@gmail.com>
2023-10-13 14:08:00 +0800

    add staging to preprocessing and add more data

--- preprocessing/combine_auxiliary.py
+++ preprocessing/combine_auxiliary.py
@@ -20,62 +20,65 @@ shopping_mall_df = pd.read_csv(shopping_mall_data)
 train_df = pd.read_csv(train_data)
 test_df = pd.read_csv(test_data)
 
-def produce_mrt_features(df, mrt_df, threshold):
+def produce_mrt_features(df, mrt_df, thresholds):
     df = df.copy()
     df = df.reset_index().rename(columns={"index": "list_id"})
     #find nearest mrt station, number of mrt within threshold t, distance to the nearest mrt
     df_explode = df.merge(mrt_df, how='cross', suffixes=('_hdb', '_mrt'))
-    df_explode['mrt_dist'] = df_explode.progress_apply(lambda x: haversine(x['latitude_hdb'], x['longitude_hdb'], x['latitude_mrt'], x['longitude_mrt']), axis=1)
+    df_explode[f'mrt_dist'] = df_explode.progress_apply(lambda x: haversine(x['latitude_hdb'], x['longitude_hdb'], x['latitude_mrt'], x['longitude_mrt']), axis=1)
     
     # cacluate nearest
     nearest_mrt_dist = df_explode.iloc[df_explode.groupby('list_id')['mrt_dist'].idxmin()][['code', 'name', 'opening_year', 'mrt_dist']].reset_index(drop=True)
     
     # count mrt within threshold
-    near_mrt_count = df_explode.groupby('list_id')['mrt_dist'].progress_apply(lambda x: (x <= threshold).sum()).reset_index(name='near_count')
+    for threshold in thresholds:
+        near_mrt_count = df_explode.groupby('list_id')['mrt_dist'].progress_apply(lambda x: (x <= threshold).sum()).reset_index(name='near_count')
+        df[f'near_mrt_count_{threshold}'] = near_mrt_count['near_count']
     nearest_mrt_dist = nearest_mrt_dist.rename(columns={"code": "nearest_code", "name": "nearest_mrt_name", "opening_year": "nearest_open_year", "mrt_dist": "nearest_mrt_dist"})
     
     # appending data
-    df['near_mrt_count'] = near_mrt_count['near_count']
     df = pd.concat([df, nearest_mrt_dist], axis='columns')
     
     return df
 
-def produce_school_features(df, school_df, threshold):
+def produce_school_features(df, school_df, thresholds):
     df = df.copy()
     df = df.reset_index().rename(columns={"index": "list_id"})
     #find nearest mrt station, number of mrt within threshold t, distance to the nearest mrt
     df_explode = df.merge(school_df, how='cross', suffixes=('_hdb', '_school'))
-    df_explode['school_dist'] = df_explode.progress_apply(lambda x: haversine(x['latitude_hdb'], x['longitude_hdb'], x['latitude_school'], x['longitude_school']), axis=1)
+    df_explode[f'school_dist'] = df_explode.progress_apply(lambda x: haversine(x['latitude_hdb'], x['longitude_hdb'], x['latitude_school'], x['longitude_school']), axis=1)
     
     # cacluate nearest
     nearest_school_dist = df_explode.iloc[df_explode.groupby('list_id')['school_dist'].idxmin()][['name', 'school_dist']].reset_index(drop=True)
     
     # count mrt within threshold
-    near_mrt_count = df_explode.groupby('list_id')['school_dist'].progress_apply(lambda x: (x <= threshold).sum()).reset_index(name='near_count')
+    for threshold in thresholds:
+        near_mrt_count = df_explode.groupby('list_id')['school_dist'].progress_apply(lambda x: (x <= threshold).sum()).reset_index(name='near_count')
+        df[f'near_school_count_{threshold}'] = near_mrt_count['near_count']
     nearest_school_dist = nearest_school_dist.rename(columns={"name": "nearest_school_name", "school_dist": "nearest_school_dist"})
     
     # appending data
-    df['near_school_count'] = near_mrt_count['near_count']
     df = pd.concat([df, nearest_school_dist], axis='columns')
     
     return df
 
-def produce_mall_features(df, mall_df, threshold):
+def produce_mall_features(df, mall_df, thresholds):
     df = df.copy()
     df = df.reset_index().rename(columns={"index": "list_id"})
     #find nearest mrt station, number of mrt within threshold t, distance to the nearest mrt
     df_explode = df.merge(mall_df, how='cross', suffixes=('_hdb', '_mall'))
-    df_explode['mall_dist'] = df_explode.progress_apply(lambda x: haversine(x['latitude_hdb'], x['longitude_hdb'], x['latitude_mall'], x['longitude_mall']), axis=1)
+    df_explode[f'mall_dist'] = df_explode.progress_apply(lambda x: haversine(x['latitude_hdb'], x['longitude_hdb'], x['latitude_mall'], x['longitude_mall']), axis=1)
     
     # cacluate nearest
     nearest_mall_dist = df_explode.iloc[df_explode.groupby('list_id')['mall_dist'].idxmin()][['name', 'mall_dist']].reset_index(drop=True)
     
     # count mrt within threshold
-    near_mall_count = df_explode.groupby('list_id')['mall_dist'].progress_apply(lambda x: (x <= threshold).sum()).reset_index(name='near_count')
+    for threshold in thresholds:
+        near_mall_count = df_explode.groupby('list_id')['mall_dist'].progress_apply(lambda x: (x <= threshold).sum()).reset_index(name='near_count')
+        df[f'near_mall_count_{threshold}'] = near_mall_count['near_count']
     nearest_mall_dist = nearest_mall_dist.rename(columns={"name": "nearest_mall_name", "mall_dist": "nearest_mall_dist"})
     
     # appending data
-    df['near_mall_count'] = near_mall_count['near_count']
     df = pd.concat([df, nearest_mall_dist], axis='columns')
     
     return df
--- preprocessing/process_auxiliary.py
+++ preprocessing/process_auxiliary.py
@@ -5,42 +5,83 @@ sys.path.append(os.getcwd())
 import pandas as pd
 from preprocessing.data_processing import *
 # from tqdm import tqdm
-from preprocessing.combine_auxiliary import produce_mall_features, produce_mrt_features, produce_school_features
+from preprocessing.combine_auxiliary import produce_mall_features, produce_mrt_features, produce_school_features, produce_coe_features
 
 # tqdm.pandas()
 
 existing_mrt_data = './data/auxiliary-data/sg-mrt-existing-stations.csv'
 school_data = './data/auxiliary-data/sg-primary-schools.csv'
 shopping_mall_data = './data/auxiliary-data/sg-shopping-malls.csv'
+coe_data = './data/auxiliary-data/sg-coe-prices.csv'
 train_data = './data/train.csv'
 test_data = './data/test.csv'
 
 mrt_df = pd.read_csv(existing_mrt_data)
 school_df = pd.read_csv(school_data)
 shopping_mall_df = pd.read_csv(shopping_mall_data)
+coe_df = pd.read_csv(coe_data)
 
+mrt_radius = [0.3, 0.8, 1.5, 2] # from analysis
+mall_radius = [0.5, 1.0, 1.5, 2]
+school_radius = [0.5, 1.0, 1.5, 2]
 train_df = pd.read_csv(train_data)
 test_df = pd.read_csv(test_data)
 
-train_with_mrt = produce_mrt_features(train_df, mrt_df, 1)
-train_with_mrt.to_csv('../data/train_with_mrt.csv')
-test_with_mrt = produce_mrt_features(test_df, mrt_df, 1)
-train_with_mrt.to_csv('../data/test_with_mrt.csv')
+print('generating mrt feature with radius {}'.format(mrt_radius))
+if not os.path.exists('./data/train_with_mrt.csv'):
+    if 'list_id' in train_df:
+        train_df = train_df.drop(columns=['list_id'])
+        test_df = test_df.drop(columns=['list_id'])
+    train_df = produce_mrt_features(train_df, mrt_df, mrt_radius)
+    train_df.to_csv('./data/train_with_mrt.csv')
+    test_df = produce_mrt_features(test_df, mrt_df, mrt_radius)
+    test_df.to_csv('./data/test_with_mrt.csv')
+else:
+    train_df, test_df = pd.read_csv('./data/train_with_mrt.csv'), pd.read_csv('./data/test_with_mrt.csv')
+    print('mrt feature exists and loaded')
+    
 
-if 'list_id' in train_with_mrt.columns:
-    train_with_mrt = train_with_mrt.drop(columns=['list_id'])
-    test_with_mrt = test_with_mrt.drop(columns=['list_id'])
-# train_with_mrt.columns
-train_with_mrt_mall = produce_mall_features(train_with_mrt, shopping_mall_df, 1)
-train_with_mrt_mall.to_csv('../data/train_with_mrt_mall.csv')
-test_with_mrt_mall = produce_mall_features(test_with_mrt, shopping_mall_df, 1)
-test_with_mrt_mall.to_csv('../data/test_with_mrt_mall.csv')
+print('generating mall feature with radius {}'.format(mall_radius))
+if not os.path.exists('./data/train_with_mrt_mall.csv'):
+    if 'list_id' in train_df:
+        train_df = train_df.drop(columns=['list_id'])
+        test_df = test_df.drop(columns=['list_id'])
+    # train_with_mrt.columns
+    train_df = produce_mall_features(train_df, shopping_mall_df, mall_radius)
+    train_df.to_csv('./data/train_with_mrt_mall.csv')
+    test_df = produce_mall_features(test_df, shopping_mall_df, mall_radius)
+    test_df.to_csv('./data/test_with_mrt_mall.csv')
+else:
+    train_df, test_df = pd.read_csv('./data/train_with_mrt_mall.csv'), pd.read_csv('./data/test_with_mrt_mall.csv')
+    print('mall feature exists and loaded')
 
-if 'list_id' in train_with_mrt_mall:
-    train_with_mrt_mall = train_with_mrt_mall.drop(columns=['list_id'])
-    test_with_mrt_mall = test_with_mrt_mall.drop(columns=['list_id'])
-train_with_mrt_mall.columns
-train_with_mrt_mall_school = produce_school_features(train_with_mrt_mall, school_df, 1)
-train_with_mrt_mall_school.to_csv('../data/train_with_mrt_mall_school.csv')
-test_with_mrt_mall_school = produce_school_features(test_with_mrt_mall, school_df, 1)
-test_with_mrt_mall_school.to_csv('../data/test_with_mrt_mall_school.csv')
+print('generating school feature with radius {}'.format(school_radius))
+if not os.path.exists('./data/train_with_mrt_mall_school.csv'):
+    if 'list_id' in train_df:
+        train_df = train_df.drop(columns=['list_id'])
+        test_df = test_df.drop(columns=['list_id'])
+
+    train_df = produce_school_features(train_df, school_df, school_radius)
+    train_df.to_csv('./data/train_with_mrt_mall_school.csv')
+    test_df = produce_school_features(test_df, school_df, school_radius)
+    test_df.to_csv('./data/test_with_mrt_mall_school.csv')
+else:
+    train_df, test_df = pd.read_csv('./data/train_with_mrt_mall_school.csv'), pd.read_csv('./data/test_with_mrt_mall_school.csv')
+    print('school feature exists and loaded')
+
+print('generating coe feature')
+if not os.path.exists('./data/train_with_mrt_mall_school_coe.csv'):
+    if 'list_id' in train_df:
+        train_df = train_df.drop(columns=['list_id'])
+        test_df = test_df.drop(columns=['list_id'])
+
+    train_df = produce_coe_features(train_df, coe_df)
+    train_df.to_csv('./data/train_with_mrt_mall_school_coe.csv')
+    test_df = produce_coe_features(test_df, coe_df)
+    test_df.to_csv('./data/test_with_mrt_mall_school_coe.csv')
+else:
+    train_df, test_df = pd.read_csv('./data/train_with_mrt_mall_school_coe.csv'), pd.read_csv('./data/test_with_mrt_mall_school_coe.csv')
+    print('coe feature exists and loaded')
+
+train_df.to_csv('./data/train_final.csv')
+test_df.to_csv('./data/test_final.csv')
