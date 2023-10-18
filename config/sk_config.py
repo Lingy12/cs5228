b0a37eaec5292913596be012ea9c4cb81e891edd
lgy_workstation <you@example.com>
2023-10-18 14:15:39 +0800

    add new data

--- config/sk_config.py
+++ config/sk_config.py
@@ -1,4 +1,5 @@
-from sklearn.preprocessing import StandardScaler, Normalizer, MinMaxScaler, OneHotEncoder
+from sklearn.preprocessing import StandardScaler, Normalizer, MinMaxScaler, OneHotEncoder, OrdinalEncoder
+from sklearn.impute import SimpleImputer
 from sklearn.neural_network import MLPRegressor
 from sklearn.decomposition import PCA
 
@@ -21,11 +22,34 @@ base_mlp_conf = {
     "target": 'monthly_rent',
     "pipelines": {'num_processor': [StandardScaler()], 
                     'ord_cat_processor': [OneHotEncoder(sparse_output=False)], 
-                    'norm_cat_processor': [OneHotEncoder(sparse_output=False), PCA(n_components='mle')], # ensure the concatenation 
+                    'norm_cat_processor': [OneHotEncoder(sparse_output=False)], # ensure the concatenation 
                     'model': [MLPRegressor(random_state=1, max_iter=1000, hidden_layer_sizes=(200,), 
                                                 learning_rate='adaptive', early_stopping=True, validation_fraction=0.01)]}
 }
 
+base_mlp_pca = {
+    "features": ['floor_area_sqm',
+                         'age',
+                         'town_psqm',
+                         'regional_psqm',  
+                         'nearest_mrt_dist',
+                         'nearest_mall_dist',
+                         'nearest_school_dist',
+                        'near_mrt_count_1.0',
+                         'near_mall_count_1.0',
+                         'near_school_count_1.0',
+                         'date',
+                        #  'price'
+                         ],
+    "norm_features": ['flat_type', 'flat_model', 'region', 'subzone', 'planning_area', 'town'],
+    "ord_features": [],
+    "target": 'monthly_rent',
+    "pipelines": {'num_processor': [SimpleImputer(), StandardScaler()], 
+                    'ord_cat_processor': [OrdinalEncoder()], 
+                    'norm_cat_processor': [OneHotEncoder(sparse_output=False), PCA(n_components='mle')], # ensure the concatenation 
+                    'model': [MLPRegressor(random_state=1, max_iter=1000, hidden_layer_sizes=(200,), 
+                                                learning_rate_init=0.01, learning_rate='adaptive', early_stopping=True, tol=0.001)]}
+}
 # best_mlp_conf = {
 #     "features": ['floor_area_sqm',
 #                          'age',
