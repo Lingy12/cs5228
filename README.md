1bfcaf9c5707e5de192809846315c9c5f1b0280b
Lingy12 <russellgy1229@gmail.com>
2023-11-02 20:09:36 +0800

    update documentation

--- README.md
+++ README.md
@@ -12,25 +12,37 @@ Running script need to be triggered at top level, because relative path is used.
 python preprocessing/process_auxiliary.py
 ```
 
-The pre-generated data is alread in data/train_final.csv and data/test_final.csv. You only need to run this if you have additional features add to the final dataframe. It's important that to not shuffle test.csv, because kaggle submission depends on the ordering. 
+The pre-generated data is alread in data/train_final.csv and data/test_final.csv. You only need to run this if you have additional features add to the final dataframe. It's important that to not shuffle test.csv, because kaggle submission depends on the ordering.
 
 ### Model selection using k_fold
 
 ```python
-python training/k_fold.py base_conf 
-```
-
-Add your new configuration in config/sk_config.py, and replace the "base_conf" with your new config name. 
-
-### Generate prediction
-
-```python
-python prediction/sk_prediction.py base_conf 
+python training/k_fold.py <base_conf>
 ```
 
 Add your new configuration in config/sk_config.py, and replace the "base_conf" with your new config name.
 
-### Alternative: Pytorch training (For GPU acceleration only)
+### Generate prediction
+
+```python
+python prediction/sk_prediction.py <base_conf> 
+```
+
+Add your new configuration in config/sk_config.py, and replace the "base_conf" with your new config name.
+
+### Benchmark
+
+```python
+python benchmarks.py
+
+python benchmark_with_conf_gen.py <generator_name> <run_name>
+```
+
+This script "benchmarks.py" runs all configuration in config/sk_config.py. Which produce the table showing linear regression result and MLP result.
+
+The script "benchmark_with_conf_gen.py" runs a configuration generator in config/config_generator.py. run_name is defined by user for the purpose of this configuration.
+
+### Alternative: Pytorch training (For GPU acceleration only) (Not mained further)
 
 ```python
 python training/nn_pipe.py True 500 base_conf base_model_conf BaseMLPRegressor 0.001 128 0
--- config/config_generator.py
+++ config/config_generator.py
@@ -2,51 +2,6 @@ from sklearn.preprocessing import MinMaxScaler, StandardScaler
 from sklearn.linear_model import LinearRegression
 from sklearn.neural_network import MLPRegressor
 import itertools
-def get_mlp_hp_conf():
-    hidden_layer_lst = [50, 100, 200, 500, 1000] 
-    batch_size_lst = [200]
-    init_learning_rate_lst = [0.001]
-    early_stopping_lst = [True]
-    learning_rate_lst = ['adaptive']
-    scaler_lst = [None, StandardScaler(), MinMaxScaler()]
-    tol_lst = [1e-4]
-    validate_fraction_lst = [0.1]
-
-    params = list(itertools.product(*[hidden_layer_lst, batch_size_lst, init_learning_rate_lst, early_stopping_lst, learning_rate_lst, scaler_lst, tol_lst, validate_fraction_lst]))
-    configs = []
-    names = []
-
-    conf_template = {
-        "features": ['floor_area_sqm',
-                         'age',
-                         'town_psqm',
-                         'regional_psqm',  
-                         'nearest_mrt_dist',
-                          'near_mrt_count_1.0',
-                         'near_mall_count_1.0',
-                         'near_school_count_1.0', 
-                         'nearest_mall_dist',
-                         'nearest_school_dist',
-                         'date',
-                        #  'price'
-                         ],
-    "cat_features": ['flat_type', 'flat_model', 'region', 'subzone', 'planning_area', 'town'],
-    "target": 'monthly_rent',
-    "pipelines": []
-        }
-    
-    for param in params:
-        pipeline = []
-        hidden_layer, batch_size, init_learning_rate, early_stopping, learning_rate, scaler, tol, validate_fraction = param
-        if scaler:
-            pipeline.append(scaler)
-        pipeline.append(MLPRegressor(random_state=1, hidden_layer_sizes=(hidden_layer, ), batch_size=batch_size, learning_rate_init=init_learning_rate, 
-                                     early_stopping=early_stopping, learning_rate=learning_rate, tol=tol, validation_fraction=validate_fraction, max_iter=1000))
-        conf_template['pipelines'] = pipeline
-        configs.append(conf_template.copy())
-        names.append('_'.join([str(hidden_layer), str(batch_size), str(learning_rate), str(init_learning_rate), str(early_stopping), str(tol), str(validate_fraction)]))
-
-    return list(zip(configs, names))
 
 def get_scaler_conf():
     model = [LinearRegression(), MLPRegressor(random_state=1, hidden_layer_sizes=200, batch_size=200, learning_rate_init=0.001, early_stopping=True, learning_rate='adaptive', max_iter=1000)]
